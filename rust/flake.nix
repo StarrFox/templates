958bcb251654278dca9ec37c12621d367daa4c1c
StarrFox <starrfox6312@gmail.com>
2023-04-08 12:03:14 -0500

    update rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,11 +1,32 @@
 {
   inputs = {
-    nixCargoIntegration.url = "github:yusdacra/nix-cargo-integration";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
-    root = ./.;
-    enablePreCommitHooks = true;
-    buildPlatform = "naersk";
-  };
+  outputs = {self, flake-utils, naersk, nixpkgs}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+        project_cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+      in rec {
+        defaultPackage = naersk'.buildPackage {
+          name = throw "Need to set name";
+          src = ./.;
+          # extra built inputs here
+          buildInputs = with pkgs; [];
+          version = project_cargo.package.version;
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [rustc cargo];
+        };
+      }
+    );
 }
